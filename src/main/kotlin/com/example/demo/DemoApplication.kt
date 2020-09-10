32bcd04bf6a6169b9bfc7e2631774ccf1e57a526
letsbe <letsbe@socar.kr>
2020-09-10 15:28:57 +0900

    Remove 'Logger Function for DI' from DemoApplication

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -3,7 +3,6 @@ package com.example.demo
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.ApplicationContext
-import org.springframework.context.ConfigurableApplicationContext
 
 @SpringBootApplication
 class DemoApplication
@@ -12,8 +11,4 @@ private var applicationContext: ApplicationContext? = null
 
 fun main(args: Array<String>) {
     applicationContext = runApplication<DemoApplication>(*args)
-    val allBeanNames = (applicationContext as ConfigurableApplicationContext).getBeanDefinitionNames()
-    for (beanName in allBeanNames) {
-        println(beanName)
-    }
 }
