d8e21bf60546c83fd6763f2dc104097b235e36b9
letsbe <letsbe@socar.kr>
2020-09-10 15:30:13 +0900

    Use @RestController instead of @Controller

--- src/main/kotlin/com/example/demo/controller/GreetingController.kt
+++ src/main/kotlin/com/example/demo/controller/GreetingController.kt
@@ -1,15 +1,13 @@
 package com.example.demo.controller
 
 import com.example.demo.service.GreetingService
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 class GreetingController(
     private val greetingService: GreetingService
 ) {
     @RequestMapping("/test/greet.json")
-    @ResponseBody
     fun greeting(): String = greetingService.greet()
 }
